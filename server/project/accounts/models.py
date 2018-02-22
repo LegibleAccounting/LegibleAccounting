9130fbaa749aafd2ff09a7f509ad889f37ad0fee
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-02-22 12:32:43 -0500

    LA-9: (Server) Changes:   * Change ACCOUNT_TYPES to integer association for easier access by   index; change associated database field to SmallIntegerField to   reflect this   * Rename ACCOUNT_TYPES to ACCOUNT_CATEGORIES to better reflect the   purpose   * Rename several model fields to have less verbose / more accurate names; specify   verbose_name in cases where we want the admin to show a specific label   * Extend account/account-type name max_length to 100 (in fact one sample account   has an even longer name than this)   * Don't override primary key for Account or AccountType   * Change priority field name to 'liquidity' to better reflect the   purpose   * Change date created fields to use auto_now_add which doesn't allow   editing of the date   * Change string representation of Account and Account Type models   * Remove unneeded get_side method; is_asset already fufills the need   * Don't allow Account description to be null, use blank=True instead   (see Django docs for reasoning)   * Remove commented out / unused code   * add method account_number() to account which returns an account   number calculated using the account type liquidity and the account   relative liquidity.

--- server/project/accounts/models.py
+++ server/project/accounts/models.py
@@ -1,61 +1,50 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-# Create your models here.
-
 from django.db import models
-from django.utils import timezone
 
-ACCOUNT_TYPES = (('a', 'Asset'), ('l', 'Liability'), ('e', 'Equity'))
+NUM_ACCOUNTS_PER_ACCOUNT_TYPE = 1000
+
+ACCOUNT_CATEGORIES = (
+    (0, 'Asset'),
+    (1, 'Liability'),
+    (2, 'Equity'),
+    (3, 'Revenue'),
+    (4, 'Operating Expense')
+)
 
 class AccountType(models.Model):
-    account_type_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=60, unique=True)
-    priority = models.IntegerField(verbose_name="Priority (0 is top)", default=0)
-    category = models.CharField(max_length=1, choices=ACCOUNT_TYPES, default='a')
-    date_created = models.DateTimeField(default=timezone.now)
+    category = models.SmallIntegerField(choices=ACCOUNT_CATEGORIES)
+    name = models.CharField(max_length=100, unique=True)
+    liquidity = models.PositiveIntegerField(unique=True, verbose_name='liquidity Value (1 represents highest liquidity)')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='date Created')
+
+    def __str__(self):
+        return "{0}: {1}".format(self.liquidity, self.name)
 
     def is_asset(self):
-        return True if (self.category == 'a') else False
-
-    def get_side(self):
-        return "Left" if (self.is_asset()) else "Right"
-
-    def __str__(self):
-        return "{0}: {1}".format(self.account_type_id, self.name)
-
+        return True if (self.category == 0) else False
 
 class Account(models.Model):
-    account_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=60, unique=True)
-    category = models.ForeignKey(AccountType, on_delete=models.PROTECT)
-    priority = models.IntegerField(verbose_name="Priority (0 is top)", default=0)
-    is_active = models.BooleanField(default=False)
-    starting_value = models.DecimalField(max_digits=20, decimal_places=2, default=0)
-    comments = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(default=timezone.now)
+    account_type = models.ForeignKey(AccountType, on_delete=models.PROTECT)
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=200, blank=True)
+    relative_liquidity = models.PositiveIntegerField(verbose_name='liquidity Relative to Account Type (0 represents highest relative liquidity)')
+    initial_balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='date Created')
+    is_active = models.BooleanField(default=False, verbose_name="active?")
 
     def __str__(self):
-        return "{:03}: ".format(self.account_id) + self.name + " ${:.2f}".format(self.get_balance())
+        return 'Account #' + "{:03}: ".format(self.account_number()) + self.name + " - ${:.2f}".format(self.get_balance())
 
-    def get_side(self):
-        return self.category.get_side()
+    def is_asset(self):
+        return self.account_type.is_asset()
+
+    def account_number(self):
+        return (self.account_type.liquidity * NUM_ACCOUNTS_PER_ACCOUNT_TYPE) + self.relative_liquidity
 
     def get_balance(self):
-        type_mod = 1 if (self.category.is_asset()) else -1
-        balance = self.starting_value * type_mod
-        #if self.transactions is not None:
-         #   for t in self.transactions.all():
-          #      if t.from_journal is not None:
-           #         if t.from_journal.is_approved == 't':
-            #            balance += t.get_value()
-        return balance * type_mod if (balance != 0) else 0.00
-
-    def get_json(self):
-        return '{{"name":"{0:}", "id":{1:}, "priority":{2:}, "date":"{3:}", "active":{4:}, "value":{5:}}}'.format(
-            self.name, self.account_id, self.priority, self.date_created, 1 if self.is_active == 't' else 0,
-            self.get_balance()
-        )
-
+        # TODO: This will be updated to return a calculated balance
+        # based on all transactions that have been made to this account.
+        return self.initial_balance
 
 
