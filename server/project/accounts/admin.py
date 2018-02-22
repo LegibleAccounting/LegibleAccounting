1afc3c004b0f40279f9395be9bd7b56f84994352
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-02-22 12:31:51 -0500

    LA-9: (Server) Remove custom admin class for Accounts; only superusers can access the admin interface anyway.

--- server/project/accounts/admin.py
+++ server/project/accounts/admin.py
@@ -2,17 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
-
 from .models import Account, AccountType
 
-
-class AccountAdmin(admin.ModelAdmin):
-    fieldsets = [('Administrator Values', {'fields': ['account_id', 'name', 'priority', 'category', 'date_created']}),
-                 ('Manager Values', {'fields': ['is_active', 'starting_value', 'comments']})]
-
-
-admin.site.register(Account, AccountAdmin)
+admin.site.register(Account)
 admin.site.register(AccountType)
