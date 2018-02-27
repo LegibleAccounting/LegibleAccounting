4092f9a0795b081249c6c4444c608172b7dede28
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-02-27 00:29:04 -0500

    LA-16: (Client) Fix ReactJS error messages for AddToAccounts component.

--- client/src/components/AddToAccounts.js
+++ client/src/components/AddToAccounts.js
@@ -12,85 +12,82 @@ class AddToAccountsPage extends Component {
         }
 
         AccountTypesAPI.getAll()
-        .then(data => {
-        	this.setState({
-        		accountTypes: data
-        	});
-        });
+            .then(data => {
+            	this.setState({
+            		accountTypes: data
+            	});
+            });
     }
 
     render() {
         return (
         	<div className="addToAccounts">	
-        		<h1 class="page-title">Add Account</h1>
-                <div class="row">
-					<div class="textColumn container">
+        		<h1 className="page-title">Add Account</h1>
+                <div className="row">
+					<div className="textColumn container">
 						Account Type
 					</div>
-					<div class="fieldColumn container">
-						<select class="form-control textBox">
-							{ this.state.accountTypes.length ? (
-								this.state.accountTypes.map((item, index) => (
-									<option>{item.starting_number} - {item.name}</option>
-									)
-								)
-							) : (
-								<h2>No Account Types</h2>
-							)}
+					<div className="fieldColumn container">
+						<select className="form-control textBox">
+							{
+                                this.state.accountTypes.map((item, index) => (
+                                    <option key={item.id}>{item.starting_number} - {item.name}</option>
+                                ))
+							}
 						</select>
 					</div>
 				</div>
-				<div class="row">
-					<div class="textColumn container">
+				<div className="row">
+					<div className="textColumn container">
 						Account Name
 					</div>
-					<div class="fieldColumn container">
+					<div className="fieldColumn container">
 						<input 	type="text"
 								name="accountNameTextField"
-								class="form-control textBox"
-								maxlength="200"/>
+								className="form-control textBox"
+								maxLength="200"/>
 					</div>
 				</div>	
-				<div class="row">
-					<div class="textColumn container">
+				<div className="row">
+					<div className="textColumn container">
 						Account Active
 					</div>
-					<div class="fieldColumn container">
+					<div className="fieldColumn container">
 						<input 	type="checkbox" 
 								name="accountActiveCheckBox"
-								class="accountActiveCheckBox"
+								className="accountActiveCheckBox"
 								value="true"
 								defaultChecked="checked"/>
 					</div>
 				</div>
-				<div class="row">
-					<div class="textColumn container">
+				<div className="row">
+					<div className="textColumn container">
 						Initial Balance
 					</div>
-					<div class="fieldColumn container">
+					<div className="fieldColumn container">
 						<input	type="number"
 								name="initialBalanceTextField"
-								class="form-control initialBalanceTextField currency"
+								className="form-control initialBalanceTextField currency"
 								placeholder="$0.00"
 								step="0.01"
 								min="0"/>
 					</div>
 				</div>
-				<div class="row">
-					<div class="textColumn container">
+				<div className="row">
+					<div className="textColumn container">
 						Comments
 					</div>
-					<div class="fieldColumn container">
+					<div className="fieldColumn container">
 						<input 	type="text"
 								name="commentsTextField"
-								class="form-control textBox"
-								maxlength="200"/>
+								className="form-control textBox"
+								maxLength="200"/>
 					</div>
 				</div>	
 				<div>
 					<input type="submit" value="Create" name="createButton" className="btn btn-primary createButton"/>
 				</div>
-        		<div class="fillSpace"></div>
+        		<div className="fillSpace"></div>
 				<div>
 					<NavLink className="NavLink btn btn-primary newButton" to="/accounts">&lt; Back to Accounts</NavLink>
 				</div>
