5e947263898c6dad27a4d3a0c37fd82ee706acec
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-03-05 22:26:08 -0500

    LA-38: (Server) Revert change to Account string representation as we rely on this representation in the event log UI.

--- server/project/accounts/models.py
+++ server/project/accounts/models.py
@@ -46,7 +46,7 @@ class Account(models.Model):
     is_active = models.BooleanField(default=False, verbose_name="active?")
 
     def __str__(self):
-        return 'Account #' + "{:03}: ".format(self.account_number()) + self.name + " - ${:.2f}".format(self.get_balance())
+        return 'Account #' + "{:03}: ".format(self.account_number()) + self.name
 
     def is_debit(self):
         return self.account_type.is_debit()
