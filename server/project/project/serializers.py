221db00f6bad40c5fe3c00bf165f9b8057102c9b
William <wsilloway@gmail.com>
2018-03-25 15:03:20 -0400

    Modified User Serializer to allow for better creation and modification

--- server/project/project/serializers.py
+++ server/project/project/serializers.py
@@ -3,10 +3,12 @@ from django.contrib.auth.models import User, Group
 
 from rest_framework import serializers
 
+
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('name',)
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,6 +16,35 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'first_name', 'last_name', 'username', 'groups', 'last_login', 'is_active',)
 
     groups = GroupSerializer(many=True)
+
+    def create(self, validated_data):
+        acc = User(first_name=validated_data['first_name'],
+                   last_name=validated_data['last_name'],
+                   username=validated_data['username'],
+                   is_active=validated_data['is_active'],)
+
+        groups = validated_data.pop('groups')
+        for group in groups:
+            acc.groups.add(Group.objects.get(pk=group))
+
+        acc.save()
+        return acc
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data['first_name']
+        instance.last_name = validated_data['last_name']
+        instance.username = validated_data['username']
+        instance.is_active = validated_data['is_active']
+
+        instance.groups.clear()
+
+        groups = validated_data.pop('groups')
+        for group in groups:
+            instance.groups.add(Group.objects.get(pk=group))
+
+        instance.save()
+        return instance
+
 
 class LogEntrySerializer(serializers.ModelSerializer):
     class Meta:
