2a0fdbda8ab8fb54109183a4aef740684b2d69b4
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2018-04-15 18:05:36 -0400

    LA-129: (Client) Dashboard: show ratios on dashboard.

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,21 +7,73 @@ class Dashboard extends Component {
     	super(props);
 
     	this.state = {
-            currentRatio: []
+            currentRatio: {},
+            returnOnAssets: {},
+            returnOnEquity: {},
+            netProfitMargin: {},
+            assetTurnover: {},
+            quickRatio: {}
         };
 
-        this.state.currentRatio = DashboardAPI.getCurrentRatio().then(
-        	(ratio_info) => {
+        DashboardAPI.getCurrentRatio()
+            .then((ratio_info) => {
         		this.setState({currentRatio: ratio_info});
         	});
+
+        DashboardAPI.getReturnOnAssets()
+                .then((ratio_info) => {
+                    this.setState({ returnOnAssets: ratio_info});
+                });
+
+        DashboardAPI.getReturnOnEquity()
+                .then((ratio_info) => {
+                    this.setState({ returnOnEquity: ratio_info});
+                });
+
+        DashboardAPI.getNetProfitMargin()
+                .then((ratio_info) => {
+                    this.setState({ netProfitMargin: ratio_info});
+                });
+
+        DashboardAPI.getAssetTurnover()
+            .then((ratio_info) => {
+                this.setState({ assetTurnover: ratio_info});
+            });
+        
+        DashboardAPI.getQuickRatio()
+            .then((ratio_info) => {
+                this.setState({ quickRatio: ratio_info});
+            });
     }
 
     render() {
         return (
-        	<div>
-        		<h1>{this.state.currentRatio.status}</h1>
-        		<h2>{this.state.currentRatio.ratio}</h2>
-        	</div>
+            <div>
+            	<div>
+            		<h1>{this.state.currentRatio.status}</h1>
+            		<h2>{this.state.currentRatio.ratio}</h2>
+            	</div>
+                <div>
+                    <h1>{this.state.returnOnAssets.status}</h1>
+                    <h2>{this.state.returnOnAssets.ratio}</h2>
+                </div>
+                <div>
+                    <h1>{this.state.returnOnEquity.status}</h1>
+                    <h2>{this.state.returnOnEquity.ratio}</h2>
+                </div>
+                <div>
+                    <h1>{this.state.netProfitMargin.status}</h1>
+                    <h2>{this.state.netProfitMargin.ratio}</h2>
+                </div>
+                <div>
+                    <h1>{this.state.assetTurnover.status}</h1>
+                    <h2>{this.state.assetTurnover.ratio}</h2>
+                </div>
+                <div>
+                    <h1>{this.state.quickRatio.status}</h1>
+                    <h2>{this.state.quickRatio.ratio}</h2>
+                </div>
+            </div>
             /*<ChartOfAccounts />*/
         );
     }
